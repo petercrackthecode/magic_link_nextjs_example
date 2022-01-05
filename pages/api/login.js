754829a5298bdfe2d3f8728c8e54128942046a3d
petercrackthecode <nguyenvandung02051999@gmail.com>
2022-01-05 15:33:06 -0800

    add cookies to maintain sessions | WIP

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,8 +1,13 @@
+import {Magic} from '@magic-sdk/admin';
+
+let magic = new Magic(process.env.MAGIC_SECRET_KEY);
+
 export default handler = async (req, res) => {
   if (req.method !== `POST`) return res.status(405).end(`Method not allowed`);
 
   // exchange the DID from Magic for some user data
-  // TODO
+  const did = magic.utils.parseAuthorizationHeader(req.headers.authorization);
+  const user = await magic.users.getMetadataByToken(did);
 
   // Author a couple of cookies to persist a users session
   // TODO
